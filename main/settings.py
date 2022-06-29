f4b448e094810692cd61560bc72d1ddf8197a251
gift07 <giftedwards9gmail.com>
2022-06-30 00:16:09 +0300

    neww

--- main/settings.py
+++ main/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xpqe-+)rf(d9pc-!_0z!)@sm%*+^0$uxh8*(pvtxvqekpn%i$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
